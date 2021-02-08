e1bee37c4e4b4cf43ee87401722ee7e933f6a3af
FranciscoRosal <ros18676@uvg.edu.gt>
2021-02-07 20:45:33 -0600

    avances

--- init.py
+++ init.py
@@ -1,7 +1,8 @@
 from hoppers import Hoppers
 from player import Player
+from playerv2 import Player as Playerv2
 
 player1 = Player(is_ai=True, value=1, depth=1)
-player2 = Player(is_ai=True, value=-1, depth=1)
+player2 = Playerv2(is_ai=True, value=-1, depth=1)
 
 Hoppers(player1, player2)
