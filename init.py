bc6dee35d80037cdb78137db6cfeb5a45aed4d4e
FranciscoRosal <ros18676@uvg.edu.gt>
2021-02-08 23:02:56 -0600

    Avances

--- init.py
+++ init.py
@@ -4,7 +4,8 @@ from playerv2 import Player as PlayerV2
 from playerv3 import Player as PlayerV3
 
 # player1 = Player(is_ai=True, value=1, depth=1)
+# Usar PlayerV3
 player1 = PlayerV3(is_ai=True, value=1, depth=1, multiple_jumps_enabled=True)
-player2 = PlayerV2(is_ai=True, value=-1, depth=1, multiple_jumps_enabled=True)
+player2 = PlayerV2(is_ai=True, value=-1, depth=2, multiple_jumps_enabled=True)
 
 Hoppers(player1, player2)
